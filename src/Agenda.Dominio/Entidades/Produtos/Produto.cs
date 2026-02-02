0b4dcc8bfdad1a1292d76057b54f1cfa489c4a78
jefersonwinsiga7 <Jeferson@winsiga.com.br>
2026-02-02 08:30:27 -0300

    Feat: Definindo entidade de produtos

--- src/Agenda.Dominio/Entidades/Produtos/Produto.cs
+++ src/Agenda.Dominio/Entidades/Produtos/Produto.cs
@@ -1,6 +1,7 @@
 ﻿using Agenda.Dominio.Reflection;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
@@ -25,6 +26,75 @@ namespace Agenda.Aplicacao.Entidades.Produtos
         [Column("id", Order = 1, TypeName = "bigint")]
         public int ID { get; set; } = default!;
 
+        [Required(ErrorMessage = "obrigatório informar a propriedade: idempresa", AllowEmptyStrings = false)]
+        [Column("idempresa", Order = 2, TypeName = "int")]
+        public long? IdEmpresa { get; set; }
+
+        [Required(ErrorMessage = "obrigatório informar a propriedade: idusuario", AllowEmptyStrings = false)]
+        [Column("idusuario", Order = 3, TypeName = "integer")]
+        public long? IdUsuario { get; set; } = default!;
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "obrigatório informar a propriedade: descricao", AllowEmptyStrings = true)]
+        [Column("descricao", Order = 4, TypeName = "text")]
+        public string Descricao { get; set; } = default!;
+
+        [Required(ErrorMessage = "obrigatório informar a propriedade: codigo", AllowEmptyStrings = false)]
+        [Column("codigo", Order = 5, TypeName = "integer")]
+        public long? Codigo { get; set; } = default!;
+
+        [Required(ErrorMessage = "obrigatório informar a propriedade: tipo", AllowEmptyStrings = false)]
+        [Column("tipo", Order = 6, TypeName = "integer")]
+        public long? Tipo { get; set; } = default!;
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "obrigatório informar a propriedade: unidade", AllowEmptyStrings = true)]
+        [Column("unidade", Order = 7, TypeName = "text")]
+        public string Unidade { get; set; } = default!;
+
+        [Required(ErrorMessage = "obrigatório informar a propriedade: valorcusto", AllowEmptyStrings = false)]
+        [Column("valorcusto", Order = 8, TypeName = "integer")]
+        public long? ValorCusto { get; set; } = default!;
+
+        [Required(ErrorMessage = "obrigatório informar a propriedade: valorvenda", AllowEmptyStrings = false)]
+        [Column("valorvenda", Order = 9, TypeName = "integer")]
+        public long? ValorVenda { get; set; } = default!;
+
+        [Required(ErrorMessage = "obrigatório informar a propriedade: estoqueatual", AllowEmptyStrings = false)]
+        [Column("estoqueatual", Order = 10, TypeName = "integer")]
+        public long? EstoqueAtual { get; set; } = default!;
+
+        [Required(ErrorMessage = "obrigatório informar a propriedade: estoqueminimo", AllowEmptyStrings = false)]
+        [Column("estoqueminimo", Order = 11, TypeName = "integer")]
+        public long? EstoqueMinimo { get; set; } = default!;
+
+        [Required(ErrorMessage = "obrigatório informar a propriedade: controlaestoque", AllowEmptyStrings = false)]
+        [Column("controlaestoque", Order = 12, TypeName = "integer")]
+        [Range(0, 1, ErrorMessage = "só é pemitido informar 0(zero) ou 1(um)")]
+        public long? ControlaEstoque { get; set; } = default!;
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "obrigatório informar a propriedade: observacao", AllowEmptyStrings = true)]
+        [Column("observacao", Order = 13, TypeName = "text")]
+        public string Observacao { get; set; } = default!;
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "obrigatório informar a propriedade: datacriacao", AllowEmptyStrings = false)]
+        [Column("datacriacao", Order = 14, TypeName = "timestamp without time zone")]
+        public DateTime DtCriacao { get; set; } = default!;
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "obrigatório informar a propriedade: dataatualizacao", AllowEmptyStrings = false)]
+        [Column("dataatualizacao", Order = 15, TypeName = "timestamp without time zone")]
+        public DateTime DtAtualizacao { get; set; } = default!;
+
+        [DefaultValue(1)]
+        [Required(ErrorMessage = "obrigatório informar a propriedade: status", AllowEmptyStrings = false)]
+        [Column("status", Order = 16, TypeName = "tinyint")]
+        [Range(0, 1, ErrorMessage = "só é pemitido informar 0(zero) ou 1(um)")]
+        public int? status { get; set; } = default!;
 
         [NotMapped]
         [ScaffoldColumn(false)]
